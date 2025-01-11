5affeff6db977266d7faf7497b1242e10fba948e
OwenPatrik <oz6192oz@gmail.com>
2025-01-11 17:21:54 -0600

    pathfinder implementation started

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,6 +7,7 @@
 
 package frc.robot;
 
+import com.pathplanner.lib.config.RobotConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
--- src/main/java/frc/robot/subsystems/SwerveDriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveDriveSubsystem.java
@@ -5,12 +5,19 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.hardware.Pigeon2;
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.config.PIDConstants;
+import com.pathplanner.lib.config.RobotConfig;
+import com.pathplanner.lib.controllers.PPHolonomicDriveController;
+
 import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.util.WPIUtilJNI;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -44,11 +51,11 @@ public class SwerveDriveSubsystem extends SubsystemBase {
   private double mCurrentTranslationDirection = 0;
   private double mCurrentTranslationMagnitude = 0;
 
-  private SlewRateLimiter mMagnitudeLimiter =
-      new SlewRateLimiter(Constants.DriveConstants.kMagnitudeSlewRate);
-  private SlewRateLimiter mRotationLimiter =
-      new SlewRateLimiter(Constants.DriveConstants.kRotationSlewRate);
+  private SlewRateLimiter mMagnitudeLimiter = new SlewRateLimiter(Constants.DriveConstants.kMagnitudeSlewRate);
+  private SlewRateLimiter mRotationLimiter = new SlewRateLimiter(Constants.DriveConstants.kRotationSlewRate);
+  private ChassisSpeeds mSpeedDelivered;
 
+  private Pose2d mPose;
   /** Creates a new SwerveDriveSubsystem. */
   public SwerveDriveSubsystem(
       SwerveModuleIO FrontLeftDriveio,
@@ -64,6 +71,40 @@ public class SwerveDriveSubsystem extends SubsystemBase {
 
     mGyro = new Pigeon2(kPigeonID);
     mGyro.setYaw(0);
+
+    RobotConfig config;
+    try{
+      config = RobotConfig.fromGUISettings();
+    } catch (Exception e) {
+      // Handle exception as needed
+      e.printStackTrace();
+      return;
+    }
+
+    // Configure AutoBuilder last
+    AutoBuilder.configure(
+            this::getPose, // Robot pose supplier
+            this::resetPose, // Method to reset odometry (will be called if your auto has a starting pose)
+            this::getCurrentSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+            (speeds, feedforwards) -> drive(speeds.vxMetersPerSecond, speeds.vyMetersPerSecond, speeds.omegaRadiansPerSecond, false), // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds. Also optionally outputs individual module feedforwards
+            new PPHolonomicDriveController( // PPHolonomicController is the built in path following controller for holonomic drive trains
+                    new PIDConstants(5.0, 0.0, 0.0), // Translation PID constants
+                    new PIDConstants(5.0, 0.0, 0.0) // Rotation PID constants
+            ),
+            config, // The robot configuration
+            () -> {
+              // Boolean supplier that controls when the path will be mirrored for the red alliance
+              // This will flip the path being followed to the red side of the field.
+              // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
+
+              var alliance = DriverStation.getAlliance();
+              if (alliance.isPresent()) {
+                return alliance.get() == DriverStation.Alliance.Red;
+              }
+              return false;
+            },
+            this // Reference to this subsystem to set requirements
+    );
   }
 
   public void setX() {
@@ -155,6 +196,8 @@ public class SwerveDriveSubsystem extends SubsystemBase {
     double ySpeedDelivered = ySpeedCommanded * Constants.DriveConstants.kMaxSpeedMetersPerSecond;
     double rotationDelivered = mCurrentRotation * Constants.DriveConstants.kMaxAngularSpeed;
 
+    mSpeedDelivered = new ChassisSpeeds(xSpeedDelivered, ySpeedDelivered, rotationDelivered);
+
     var swerveModuleStates =
         Constants.ModuleConstants.kDriveKinematics.toSwerveModuleStates(
             mIsFieldRelative
@@ -198,6 +241,18 @@ public class SwerveDriveSubsystem extends SubsystemBase {
 
     Logger.recordOutput("Is Field Relative?", mIsFieldRelative);
     Logger.recordOutput("Gyro Angle", mGyro.getYaw().getValueAsDouble());
-    mLights.set(mIsFieldRelative ? 0.77 : 0.61);
+    mLights.set(mIsFieldRelative? 0.77: 0.61);
+  }
+
+  public Pose2d getPose() {
+    return mPose;
+  }
+
+  public void resetPose(Pose2d pose) {
+    mPose = pose;
+  }
+
+  public ChassisSpeeds getCurrentSpeeds() {
+    return mSpeedDelivered;
   }
 }
