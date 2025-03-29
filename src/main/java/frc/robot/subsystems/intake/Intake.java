a54ed6235b37729a2f888dbbcd6bbf54ec4ce226
OwenPatrik <145722866+OwenPatrik@users.noreply.github.com>
2025-03-29 13:32:33 -0500

    Increased completeness, but not yet complete intake codes.

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -1,10 +1,19 @@
 package frc.robot.subsystems.intake;
 
+import org.littletonrobotics.junction.Logger;
+
 import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.climb.ClimberIO;
 
 public class Intake extends SubsystemBase {
     IntakeIO io;
+
+    public Intake(IntakeIO IO) {
+        io = IO;
+    }
+
     private final IntakeIOInputsAutoLogged inputs = new IntakeIOInputsAutoLogged();
 
     Servo servoLeft = new Servo(1);
@@ -12,6 +21,7 @@ public class Intake extends SubsystemBase {
 
     @Override
     public void periodic() {
-
+        io.updateInputs(inputs);
+        Logger.processInputs("intake subsystem", inputs);
     }
 }
--- src/main/java/frc/robot/subsystems/intake/IntakeIOServo.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOServo.java
@@ -1,5 +1,27 @@
 package frc.robot.subsystems.intake;
 
+import edu.wpi.first.wpilibj.Servo;
+
 public class IntakeIOServo implements IntakeIO{
-    
+    private final Servo mServo;
+
+    public void updateInputs(IntakeIOInputs inputs) {
+
+    }
+
+    public void intakeIn() {
+        mServo.setPulseTimeMicroseconds(500);
+    }
+
+    public void intakeOut() {
+        mServo.setPulseTimeMicroseconds(2500);
+    }
+
+    public void intakeStop() {
+        mServo.set(0);
+    }
+
+    public void intakeHold() {
+        mServo.setPulseTimeMicroseconds(1300);
+    }
 }
