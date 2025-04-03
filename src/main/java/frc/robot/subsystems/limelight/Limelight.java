37d9b6cb4788d19dbac3a68970a15395e71f4384
OwenPatrik <145722866+OwenPatrik@users.noreply.github.com>
2025-04-03 15:35:59 -0500

    Switch to megatag 1

--- src/main/java/frc/robot/subsystems/limelight/Limelight.java
+++ src/main/java/frc/robot/subsystems/limelight/Limelight.java
@@ -19,7 +19,7 @@ public class Limelight extends SubsystemBase {
   }
 
   public void checkLimelight(int LimelightIndex) {
-    boolean useMegaTag2 = true; // set to false to use MegaTag1
+    boolean useMegaTag2 = false; // set to false to use MegaTag1
     boolean doRejectUpdate = false;
     if (useMegaTag2 == false) {
 
